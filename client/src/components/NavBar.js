d8689702784a1d9492dc1d7f52fd9ca3bc3a4651
Felix Sun <112584698+BBHarbinger@users.noreply.github.com>
2024-04-28 18:01:29 -0400

    Updated NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -44,7 +44,6 @@ export default function NavBar() {
           <NavText href='/' text='SOCCER-X' isMain />
           <NavText href='/albums' text='TEAMS' />
           <NavText href='/songs' text='NULL' />
-          <NavText href='/leagues' text='LEAGUES' />
         </Toolbar>
       </Container>
     </AppBar>
